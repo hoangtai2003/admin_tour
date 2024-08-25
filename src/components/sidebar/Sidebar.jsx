4a1116c6eeeb072d35160c7ec77f17a9836c603e
taiht <Hdt13102k3@gmail.com>
2024-08-25 21:04:07 +0700

    add class active

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,148 +1,152 @@
-import React, { useContext, useEffect, useRef } from "react";
+import React, { useContext, useRef, useState, useEffect } from "react";
 import {
   MdOutlineBarChart,
   MdOutlineClose,
   MdOutlineLogout,
 } from "react-icons/md";
-import { FaUser } from "react-icons/fa";
-import { FaHome } from "react-icons/fa";
+import { FaUser, FaHome, FaRegFileWord, FaBed, FaShoppingCart, FaHammer } from "react-icons/fa";
 import { TfiMenuAlt } from "react-icons/tfi";
 import { TiMessages } from "react-icons/ti";
 import { SlLocationPin } from "react-icons/sl";
-import { FaRegFileWord } from "react-icons/fa";
-import { FaBed } from "react-icons/fa";
-import { FaShoppingCart } from "react-icons/fa";
-import { FaHammer } from "react-icons/fa";
-
-import { Link, useNavigate } from "react-router-dom";
+import { Link, useNavigate, useLocation } from "react-router-dom";
 import "./sidebar.css";
 import { SidebarContext } from "../../context/SideBarContext";
 import { AuthContext } from "../../context/AuthContext";
 import { Button } from "reactstrap";
 
 const Sidebar = () => {
-    const navigate = useNavigate()
+    const navigate = useNavigate();
+    const location = useLocation();
     const { isSidebarOpen, closeSidebar } = useContext(SidebarContext);
-    const {dispatch} = useContext(AuthContext)
-    const navbarRef = useRef(null)
-  const logout = () => {
-    dispatch({type: "LOGOUT"})
-    navigate('/login')
-}
-  return (
-    <nav
-      className={`sidebar ${isSidebarOpen ? "sidebar-show" : ""}`}
-      ref={navbarRef}
-    >
-      <div className="sidebar-top">
-        <div className="sidebar-brand">
-          <span className="sidebar-brand-text">Travel tour.</span>
-        </div>
-        <button className="sidebar-close-btn" onClick={closeSidebar}>
-          <MdOutlineClose size={24} />
-        </button>
-      </div>
-      <div className="sidebar-body">
-        <div className="sidebar-menu">
-          <ul className="menu-list">
-            <li className="menu-item">
-              <Link to="/" className="menu-link">
-                <span className="menu-link-icon">
-                <FaHome size={18}/>
-                </span>
-                <span className="menu-link-text">Bảng điều khiển</span>
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link to="/" className="menu-link">
-                <span className="menu-link-icon">
-                  <TfiMenuAlt size={20} />
-                </span>
-                <span className="menu-link-text">Danh mục</span>
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link to="/" className="menu-link">
-                <span className="menu-link-icon">
-                  <FaRegFileWord size={20}/>
-                </span>
-                <span className="menu-link-text">Bài viết</span>
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link to="/list-location" className="menu-link">
-                <span className="menu-link-icon">
-                  <SlLocationPin size={20} />
-                </span>
-                <span className="menu-link-text">Địa điểm</span>
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link to="/list-tour" className="menu-link">
-                <span className="menu-link-icon">
-                  <MdOutlineBarChart size={20} />
-                </span>
-                <span className="menu-link-text">Tours</span>
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link to="/list-tour" className="menu-link">
-                <span className="menu-link-icon">
-                  <FaBed  size={20} />
-                </span>
-                <span className="menu-link-text">Khách sạn</span>
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link to="/list-tour" className="menu-link">
-                <span className="menu-link-icon">
-                  <FaShoppingCart  size={20} />
-                </span>
-                <span className="menu-link-text">Danh sách đặt tour</span>
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link to="/" className="menu-link">
-                <span className="menu-link-icon">
-                  <TiMessages size={20} />
-                </span>
-                <span className="menu-link-text">Quản lý bình luận</span>
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link to="/list-user" className="menu-link">
-                <span className="menu-link-icon">
-                  <FaUser size={18} />
-                </span>
-                <span className="menu-link-text">Người dùng</span>
-              </Link>
-            </li>
-            <li className="menu-item">
-              <Link to="/" className="menu-link">
-                <span className="menu-link-icon">
-                  <FaHammer size={18} />
-                </span>
-                <span className="menu-link-text">Vai trò</span>
-              </Link>
-            </li>
-          </ul>
-        </div>
+    const { dispatch } = useContext(AuthContext);
+    const navbarRef = useRef(null);
+    
+    // Sử dụng state để lưu đường dẫn hiện tại
+    const [activeLink, setActiveLink] = useState(location.pathname);
 
-        <div className="sidebar-menu sidebar-menu2">
-          <ul className="menu-list">
-            <li className="menu-item">
-              <Button type="submit" className="menu-link" onClick={logout}>
-                <span className="menu-link-icon">
-                  <MdOutlineLogout size={20} />
-                </span>
-                <span className="menu-link-text" style={{fontSize: '17px'}}>Logout</span>
-              </Button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+    useEffect(() => {
+        setActiveLink(location.pathname);
+    }, [location.pathname]);
+
+    const logout = () => {
+        dispatch({ type: "LOGOUT" });
+        navigate('/login');
+    }
+
+    return (
+        <nav
+          className={`sidebar ${isSidebarOpen ? "sidebar-show" : ""}`}
+          ref={navbarRef}
+        >
+          <div className="sidebar-top">
+            <div className="sidebar-brand">
+              <span className="sidebar-brand-text">Travel tour.</span>
+            </div>
+            <button className="sidebar-close-btn" onClick={closeSidebar}>
+              <MdOutlineClose size={24} />
+            </button>
+          </div>
+          <div className="sidebar-body">
+            <div className="sidebar-menu">
+              <ul className="menu-list">
+                <li className={`menu-item ${activeLink === "/" ? "active" : ""}`}>
+                  <Link to="/" className="menu-link" onClick={() => setActiveLink("/")}>
+                    <span className="menu-link-icon">
+                      <FaHome size={18}/>
+                    </span>
+                    <span className="menu-link-text">Bảng điều khiển</span>
+                  </Link>
+                </li>
+                <li className={`menu-item ${activeLink === "/" ? "active" : ""}`}>
+                  <Link to="/" className="menu-link" onClick={() => setActiveLink("/")}>
+                    <span className="menu-link-icon">
+                      <TfiMenuAlt size={20} />
+                    </span>
+                    <span className="menu-link-text">Danh mục</span>
+                  </Link>
+                </li>
+                <li className={`menu-item ${activeLink === "/" ? "active" : ""}`}>
+                  <Link to="/" className="menu-link" onClick={() => setActiveLink("/")}>
+                    <span className="menu-link-icon">
+                      <FaRegFileWord size={20}/>
+                    </span>
+                    <span className="menu-link-text">Bài viết</span>
+                  </Link>
+                </li>
+                <li className={`menu-item ${activeLink === "/list-location" ? "active" : ""}`}>
+                  <Link to="/list-location" className="menu-link" onClick={() => setActiveLink("/list-location")}>
+                    <span className="menu-link-icon">
+                      <SlLocationPin size={20} />
+                    </span>
+                    <span className="menu-link-text">Địa điểm</span>
+                  </Link>
+                </li>
+                <li className={`menu-item ${activeLink === "/list-tour" ? "active" : ""}`}>
+                  <Link to="/list-tour" className="menu-link" onClick={() => setActiveLink("/list-tour")}>
+                    <span className="menu-link-icon">
+                      <MdOutlineBarChart size={20} />
+                    </span>
+                    <span className="menu-link-text">Tours</span>
+                  </Link>
+                </li>
+                <li className={`menu-item ${activeLink === "/" ? "active" : ""}`}>
+                  <Link to="/" className="menu-link" onClick={() => setActiveLink("/")}>
+                    <span className="menu-link-icon">
+                      <FaBed size={20} />
+                    </span>
+                    <span className="menu-link-text">Khách sạn</span>
+                  </Link>
+                </li>
+                <li className={`menu-item ${activeLink === "/" ? "active" : ""}`}>
+                  <Link to="/" className="menu-link" onClick={() => setActiveLink("/")}>
+                    <span className="menu-link-icon">
+                      <FaShoppingCart size={20} />
+                    </span>
+                    <span className="menu-link-text">Danh sách đặt tour</span>
+                  </Link>
+                </li>
+                <li className={`menu-item ${activeLink === "/" ? "active" : ""}`}>
+                  <Link to="/" className="menu-link" onClick={() => setActiveLink("/")}>
+                    <span className="menu-link-icon">
+                      <TiMessages size={20} />
+                    </span>
+                    <span className="menu-link-text">Quản lý bình luận</span>
+                  </Link>
+                </li>
+                <li className={`menu-item ${activeLink === "/list-user" ? "active" : ""}`}>
+                  <Link to="/list-user" className="menu-link" onClick={() => setActiveLink("/list-user")}>
+                    <span className="menu-link-icon">
+                      <FaUser size={18} />
+                    </span>
+                    <span className="menu-link-text">Người dùng</span>
+                  </Link>
+                </li>
+                <li className={`menu-item ${activeLink === "/" ? "active" : ""}`}>
+                  <Link to="/" className="menu-link" onClick={() => setActiveLink("/")}>
+                    <span className="menu-link-icon">
+                      <FaHammer size={18} />
+                    </span>
+                    <span className="menu-link-text">Vai trò</span>
+                  </Link>
+                </li>
+              </ul>
+            </div>
+
+            <div className="sidebar-menu sidebar-menu2">
+              <ul className="menu-list">
+                <li className="menu-item">
+                  <Button type="submit" className="menu-link" onClick={logout}>
+                    <span className="menu-link-icon">
+                      <MdOutlineLogout size={20} />
+                    </span>
+                    <span className="menu-link-text" style={{fontSize: '17px'}}>Logout</span>
+                  </Button>
+                </li>
+              </ul>
+            </div>
+          </div>
+        </nav>
+    );
 };
 
 export default Sidebar;
