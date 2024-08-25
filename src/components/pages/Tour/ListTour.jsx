d9c7cd5397799bcb0b3e7e35286d6d8e12e50c8c
taiht <Hdt13102k3@gmail.com>
2024-08-26 01:36:27 +0700

    Pagination tour

--- src/components/pages/Tour/ListTour.jsx
+++ src/components/pages/Tour/ListTour.jsx
@@ -1,10 +1,11 @@
+import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 import LisTourAction from "./ListTourAction";
 import "../table.css";
-import { BASE_URL } from "../../../utils/config"
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
+import { BASE_URL } from "../../../utils/config";
 import { IoMdAdd } from "react-icons/io";
+
 const TABLE_HEADS = [
   "STT",
   "Tiêu đề",
@@ -15,86 +16,103 @@ const TABLE_HEADS = [
 ];
 
 const ListTour = () => {
-    const [tours, setTours] = useState([])
+    const [tours, setTours] = useState([]);
+    const [currentPage, setCurrentPage] = useState(1);
+    const [totalPages, setTotalPages] = useState(0);
+
     useEffect(() => {
         const fetchTours = async () => {
             try {
-                const response = await axios.get(`${BASE_URL}/tours`)
-                setTours(response.data.data)
+                const response = await axios.get(`${BASE_URL}/tours?page=${currentPage}`);
+                setTours(response.data.data);
+                setTotalPages(response.data.totalPages);
             } catch (error) {
                 console.error('Error fetching tours:', error);
             }
-        }
+        };
         fetchTours();
-    }, [])
+    }, [currentPage]);
+
     const stripHTML = (html) => {
-      const doc = new DOMParser().parseFromString(html, 'text/html');
-      return doc.body.textContent || "";
+        const doc = new DOMParser().parseFromString(html, 'text/html');
+        return doc.body.textContent || "";
     };
+
     const handleDelete = (id) => {
         setTours(tours.filter(tour => tour.id !== id)); 
     };
+
+    const handlePageChange = (newPage) => {
+        if (newPage >= 1 && newPage <= totalPages) {
+            setCurrentPage(newPage);
+        }
+    };
+
     return (
-      <section className="content-area-table">
-        <div className="data-table-info">
-          <h4 className="data-table-title">Danh sách Tour</h4>
-          <Link to="/add-tour" className="create"><IoMdAdd className="create_icon"/> Tạo mới</Link>
-        </div>
-        <div className="data-table-diagram">
-          <table>
-            <thead>
-              <tr>
-                {TABLE_HEADS?.map((th, index) => (
-                  <th key={index}>{th}</th>
-                ))}
-              </tr>
-            </thead>
-            <tbody>
-                {tours?.map((tour, index) => (
-                    <tr key={tour.id}>
-                        <td>{index + 1}</td>
-                        <td>{tour.name}</td>
-                        <td>
-                            <div className="image-container">
-                                <img src={tour.tour_image} alt={tour.name} className="image" />
-                            </div>
-                        </td>
-                        <td>
-                            {/* <div dangerouslySetInnerHTML={{ __html: tour.description_itinerary }} /> */}
-                            <p><b>Hành trình: </b>{tour.duration}</p>
-                            {tour.tourChildren.length > 0 && (
-                                <>
-                                <p><b>Số người: </b>{tour.tourChildren[0].total_seats}</p>
-                                <p><b>Đã đăng ký: </b>{}</p>
-                                <p><b>Giá người lớn: </b>{tour.tourChildren[0].price_adult} vnd</p>
-                                <p><b>Giá trẻ em: </b>{tour.tourChildren[0].price_child} vnd</p>
-                                </>
-                            )}
-                        </td>
-                        <td>
-                            <p><b>Địa điểm: </b>
-                                {tour.tourLocations.map((tourLocation, i) => (
-                                    <span key={i}>{tourLocation.location.name}{i < tour.tourLocations.length - 1 ? ' - ' : ''}</span>
-                                ))}
-                            </p>
-                            <p><b>Di chuyển: </b>{tour.transportations}</p>
-                            <p><b>Điểm xuất phát: </b>{tour.departure_city}</p>
-                            {tour.tourChildren.length > 0 && (
-                                <>
-                                <p><b>Ngày đi: </b>{tour.tourChildren[0].start_date}</p>
-                                <p><b>Ngày về: </b>{tour.tourChildren[0].end_date}</p>
-                                </>
-                            )}
-                        </td>
-                        <td className="dt-cell-action">
-                            <LisTourAction id={tour.id}  onDelete={handleDelete}/>
-                        </td>
-                    </tr>
-                ))}
-            </tbody>
-          </table>
-        </div>
-      </section>
+        <section className="content-area-table">
+            <div className="data-table-info">
+                <h4 className="data-table-title">Danh sách Tour</h4>
+                <Link to="/add-tour" className="create"><IoMdAdd className="create_icon"/> Tạo mới</Link>
+            </div>
+            <div className="data-table-diagram">
+                <table>
+                    <thead>
+                        <tr>
+                            {TABLE_HEADS?.map((th, index) => (
+                                <th key={index}>{th}</th>
+                            ))}
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {tours?.map((tour, index) => (
+                            <tr key={tour.id}>
+                                <td>{index + 1}</td>
+                                <td>{tour.name}</td>
+                                <td>
+                                    <div className="image-container">
+                                        <img src={tour.tour_image} alt={tour.name} className="image" />
+                                    </div>
+                                </td>
+                                <td>
+                                    <p><b>Hành trình: </b>{tour.duration}</p>
+                                    {tour.tourChildren.length > 0 && (
+                                        <>
+                                        <p><b>Số người: </b>{tour.tourChildren[0].total_seats}</p>
+                                        <p><b>Giá người lớn: </b>{tour.tourChildren[0].price_adult} vnd</p>
+                                        <p><b>Giá trẻ em: </b>{tour.tourChildren[0].price_child} vnd</p>
+                                        </>
+                                    )}
+                                </td>
+                                <td>
+                                    <p><b>Địa điểm: </b>
+                                        {tour.tourLocations.map((tourLocation, i) => (
+                                            <span key={i}>{tourLocation.location.name}{i < tour.tourLocations.length - 1 ? ' - ' : ''}</span>
+                                        ))}
+                                    </p>
+                                    <p><b>Di chuyển: </b>{tour.transportations}</p>
+                                    <p><b>Điểm xuất phát: </b>{tour.departure_city}</p>
+                                    {tour.tourChildren.length > 0 && (
+                                        <>
+                                        <p><b>Ngày đi: </b>{tour.tourChildren[0].start_date}</p>
+                                        <p><b>Ngày về: </b>{tour.tourChildren[0].end_date}</p>
+                                        </>
+                                    )}
+                                </td>
+                                <td className="dt-cell-action">
+                                    <LisTourAction id={tour.id}  onDelete={handleDelete}/>
+                                </td>
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+            {/* Pagination Controls */}
+            <div className="pagination">
+                <button onClick={() => handlePageChange(currentPage - 1)} disabled={currentPage === 1}>Previous</button>
+                <span>Page {currentPage} of {totalPages}</span>
+                <button onClick={() => handlePageChange(currentPage + 1)} disabled={currentPage === totalPages}>Next</button>
+            </div>
+        </section>
     );
 };
 
